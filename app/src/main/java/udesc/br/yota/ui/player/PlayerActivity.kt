7d5dc6f41413cc97c6978ad579f5e7c9a783c275
Nicolas <ndebacher@gmail.com>
2024-05-16 18:33:55 -0300

    playing the music

--- app/src/main/java/udesc/br/yota/ui/player/PlayerActivity.kt
+++ app/src/main/java/udesc/br/yota/ui/player/PlayerActivity.kt
@@ -1,11 +1,16 @@
 package udesc.br.yota.ui.player
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.yota.R
 import udesc.br.yota.ui.player.ui.player.PlayerFragment
 
 class PlayerActivity : AppCompatActivity() {
+
+    private lateinit var playerButton: Button
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +19,23 @@ class PlayerActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PlayerFragment.newInstance())
                 .commitNow()
+        }
+        initButtons();
+    }
+
+    private fun initButtons(){
+        playerButton = findViewById(R.id.btPlay)
+        playerButton.setOnClickListener{ _-> playAndPause() }
+    }
+
+    private fun playAndPause(){
+        if (mediaPlayer == null) mediaPlayer = MediaPlayer.create(this, R.raw.music)
+        mediaPlayer?.let { player ->
+            if (player.isPlaying) {
+                player.pause()
+            } else {
+                player.start()
+            }
         }
     }
 }
