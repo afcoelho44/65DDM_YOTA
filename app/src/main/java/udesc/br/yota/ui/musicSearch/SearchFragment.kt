3ac31a9448dd86e857683e93a6a960a998abca19
Deb4cker <ndebacher@gmail.com>
2024-05-24 20:38:21 -0300

    tentativa de retrofit

--- app/src/main/java/udesc/br/yota/ui/musicSearch/SearchFragment.kt
+++ app/src/main/java/udesc/br/yota/ui/musicSearch/SearchFragment.kt
@@ -5,16 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.yota.databinding.FragmentSearchBinding
+import retrofit2.Call
+import retrofit2.Callback
+import udesc.br.yota.Providers.ExternalMusicProvider
+import udesc.br.yota.Providers.MusicPlayerProvider
+
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
+    private lateinit var provider: ExternalMusicProvider
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -27,12 +32,47 @@ class SearchFragment : Fragment() {
 
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val searchButton = binding.searchButton
+        val textSearch = binding.textSearch
+
+        provider = ExternalMusicProvider()
+        provider.retrofitInitializer()
 
         val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        searchButton.setOnClickListener {
+            val musicName = textSearch.text.toString()
+            searchInApi(musicName)
+        }
+
         return root
+    }
+
+    private fun searchInApi(musicName: String){
+        try {
+            val call = provider.getMusic().getMusicByTitle(musicName)
+
+            call.enqueue(object : Callback<ByteArray> {
+                override fun onResponse(call: Call<ByteArray>, response: retrofit2.Response<ByteArray>) {
+                    if(response.isSuccessful){
+                        val music = response.body()
+                        MusicPlayerProvider.getInstance(null).setMusicWithByteArray(music!!)
+                    } else {
+                        Toast.makeText(activity?.application, "Não achei :(", Toast.LENGTH_LONG);
+                    }
+                }
+
+                override fun onFailure(call: Call<ByteArray>, t: Throwable) {
+                    Toast.makeText(activity?.application, "Não achei :(", Toast.LENGTH_LONG);
+                }
+            })
+
+        } catch (e: Throwable){
+            Toast.makeText(activity?.application, "Não achei :(", Toast.LENGTH_LONG);
+        }
     }
 
     override fun onDestroyView() {
