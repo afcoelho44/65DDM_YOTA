6d9c7622f9a21bde89ec1b3a1b4133962748f9f6
Nicolas <ndebacher@gmail.com>
2024-05-23 22:55:16 -0300

    funciona jesus 2.0: a vingança de ana fabia

---
+++ app/src/main/java/udesc/br/yota/ui/register/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package udesc.br.yota.ui.register
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.yota.R
+import com.example.yota.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
