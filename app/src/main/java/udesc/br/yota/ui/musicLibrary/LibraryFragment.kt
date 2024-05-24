eac7e2b11d5e678a44bc65366fba0107e2029c6e
afcoelho44 <afcoelho44@gmail.com>
2024-05-24 00:02:56 -0300

    Clicando na musica ela tocaaaaaaaaaaaa

--- app/src/main/java/udesc/br/yota/ui/musicLibrary/LibraryFragment.kt
+++ app/src/main/java/udesc/br/yota/ui/musicLibrary/LibraryFragment.kt
@@ -5,24 +5,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.R
 import com.example.yota.databinding.FragmentLibraryBinding
 import com.example.yota.databinding.FragmentSearchBinding
 import udesc.br.yota.MainActivity
+import udesc.br.yota.Providers.MusicPlayerProvider
 import udesc.br.yota.ui.model.Music
 import udesc.br.yota.ui.musicLibrary.adapter.MusicsAdapter
 import udesc.br.yota.ui.musicSearch.SearchViewModel
 import udesc.br.yota.ui.repository.MusicDao
 import udesc.br.yota.ui.repository.MusicRepository
 
-class LibraryFragment : Fragment() {
+class LibraryFragment : Fragment(), AdapterView.OnItemClickListener {
 
-    lateinit var listMusic: ListView
     lateinit var musics: List<Music>
     lateinit var repository: MusicRepository
     lateinit var adapter: MusicsAdapter
@@ -55,6 +57,9 @@ class LibraryFragment : Fragment() {
 
             listMusic.adapter= adapter
         }
+
+        listMusic.setOnItemClickListener(this)
+
         return root
     }
 
@@ -62,6 +67,14 @@ class LibraryFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val music = parent?.getItemAtPosition(position) as Music
+        val player= MusicPlayerProvider.getInstance(null)
+
+        player.setMusic(music.id)
+
     }
 
 }
